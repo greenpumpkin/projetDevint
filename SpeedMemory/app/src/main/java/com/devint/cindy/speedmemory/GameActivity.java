77e7bb2032fe45886ec14bf1a02d564d955b26aa
Cindy Najjar <najjar@polytech.unice.fr>
2015-03-10 09:02:34 +0100

    Main+Game+Help Activities

---
+++ SpeedMemory/app/src/main/java/com/devint/cindy/speedmemory/GameActivity.java
@@ -0,0 +1,59 @@
+package com.devint.cindy.speedmemory;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+
+
+public class GameActivity extends ActionBarActivity {
+
+    private Chronometer chrono;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        chrono = (Chronometer)findViewById(R.id.chronometer);
+        chrono.start();
+
+        final Button buttonHelp = (android.widget.Button) findViewById(R.id.button2);
+        buttonHelp.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GameActivity.this, HelpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_game, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
