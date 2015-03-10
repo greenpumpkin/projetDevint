d475b984369186f31b34e11bd51f3ca549328341
Cindy Najjar <najjar@polytech.unice.fr>
2015-03-10 12:27:28 +0100

    ajout d'un bouton retour dans HelpActivity+création d'une classe Card

---
+++ SpeedMemory/app/src/main/java/com/devint/cindy/speedmemory/Card.java
@@ -0,0 +1,19 @@
+package com.devint.cindy.speedmemory;
+
+/**
+ * Created by Cindy Najjar on 10/03/15.
+ */
+
+public class Card {
+
+    private int position;
+    private int numCard;
+    private boolean isReturned = false;
+
+    public Card(int position,int numCard) {
+        this.position = position;
+        this.numCard = numCard;
+    }
+
+
+}
--- SpeedMemory/app/src/main/java/com/devint/cindy/speedmemory/HelpActivity.java
+++ SpeedMemory/app/src/main/java/com/devint/cindy/speedmemory/HelpActivity.java
@@ -1,9 +1,12 @@
 package com.devint.cindy.speedmemory;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class HelpActivity extends ActionBarActivity {
@@ -12,6 +15,16 @@ public class HelpActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        final Button buttonHelp = (android.widget.Button) findViewById(R.id.button3);
+        buttonHelp.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HelpActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
