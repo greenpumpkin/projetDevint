55b1d820da8a3057abfeb5a898ae24cf52ae24e5
JeanLoic Mugnier <mj207478@etu.unice.fr>
2015-03-11 08:01:07 +0100

    creation de la classe game + ajout méthodes et attributs classe Card

--- SpeedMemory/app/src/main/java/com/devint/cindy/speedmemory/Card.java
+++ SpeedMemory/app/src/main/java/com/devint/cindy/speedmemory/Card.java
@@ -1,18 +1,21 @@
 package com.devint.cindy.speedmemory;
 
+
 /**
  * Created by Cindy Najjar on 10/03/15.
  */
 
 public class Card {
 
+    private String uri;
     private int position;
-    private int numCard;
+    private String color;
+    private int id;
     private boolean isReturned = false;
 
-    public Card(int position,int numCard) {
+    public Card(int position,int id) {
         this.position = position;
-        this.numCard = numCard;
+        this.id = id;
     }
 
 
