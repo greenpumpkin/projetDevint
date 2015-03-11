4bd1f290c439ed54afaecf536a041ae5403b6b72
Cindy Najjar <najjar@polytech.unice.fr>
2015-03-11 10:02:27 +0100

    modif GameActivity

--- SpeedMemory/app/src/main/java/com/devint/cindy/speedmemory/GameActivity.java
+++ SpeedMemory/app/src/main/java/com/devint/cindy/speedmemory/GameActivity.java
@@ -1,5 +1,6 @@
 package com.devint.cindy.speedmemory;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.SystemClock;
 import android.support.v7.app.ActionBarActivity;
@@ -7,21 +8,33 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class GameActivity extends ActionBarActivity {
 
     private Chronometer chrono;
+    private GridView grid;
+    private List listImages = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        grid = (GridView)findViewById(R.id.gridView);
+        grid.setAdapter(new ImageAdapter(this));
         chrono = (Chronometer)findViewById(R.id.chronometer);
         chrono.start();
+
 
     }
 
@@ -46,5 +59,51 @@ public class GameActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+
+        public int getCount() {
+            return mThumbIds.length;
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ImageView imageView;
+            if (convertView == null) {  // if it's not recycled, initialize some attributes
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                imageView.setPadding(8, 8, 8, 8);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+            imageView.setImageResource(mThumbIds[position]);
+            return imageView;
+        }
+
+        // references to our images
+        private Integer[] mThumbIds = {
+                R.drawable.speed_memory_card,
+                R.drawable.speed_memory_card,
+                R.drawable.speed_memory_card,
+                R.drawable.speed_memory_card,
+                R.drawable.speed_memory_card,
+                R.drawable.speed_memory_card
+        };
     }
 }
